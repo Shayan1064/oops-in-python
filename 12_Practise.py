c83058d0c32c259c884ae7b9b4b57b204cd6d754
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-29 23:08:17 +0500

    Concepts related to OOP

---
+++ 12_Practise.py
@@ -0,0 +1,18 @@
+# write culculator which find the square cube of number
+
+class culculator:
+    Welcome="Welcome to Shayan's Culculator"
+
+    def __init__(self,number):
+        self.numebr=number
+
+    def square(self):
+        print("Sqaure : ",self.number*self.number)
+
+    def cube(self):
+        print("Cube : ",self.number*self.number*self.number)
+
+s1=culculator(4)
+s1.square
+c1=culculator(4)
+c1.cube
