26958e75efc8dbcc0d60d49b05a210b928c7695d
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-08-29 23:07:31 +0500

    Concpets related to OOP

---
+++ 11_Practise.py
@@ -0,0 +1,25 @@
+class compnay:
+    compnay_name="Google"
+    def __init__(self,name,salary):
+        self.name=name
+        self.salary=salary
+
+p1=compnay("Shayan","100k $")
+print("1):Name  ",p1.name)
+print(p1.name,"'s Salary : ",p1.salary)
+
+p1=compnay("Waqas","100k $")
+print("1):Name  ",p1.name)
+print(p1.name,"'s Salary : ",p1.salary)
+
+p1=compnay("Umair","100k $")
+print("1):Name  ",p1.name)
+print(p1.name,"'s Salary : ",p1.salary)
+
+p1=compnay("Awais","100k $")
+print("1):Name  ",p1.name)
+print(p1.name,"'s Salary : ",p1.salary)
+
+p1=compnay("Zain","100k $")
+print("1):Name  ",p1.name)
+print(p1.name,"'s Salary : ",p1.salary)
